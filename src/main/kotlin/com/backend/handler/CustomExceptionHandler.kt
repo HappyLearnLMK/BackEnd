2313421c912fac64fe261b1970243dae108a5bc5
eco_min <guldor10.b@gmail.com>
2023-04-16 01:45:26 +0900

    feat(Handler): Aop, Exception 기능 개발
    CustomValidAop.kt
    - postMapping 으로 들어오는 BindingResult error 를 걸러내기 위한 class
    CustomValidationException.kt
    - Validation Exception class
    CustomApiException.kt
    - Api 오류 (사용자 입력 문제, 내부 문제로 발생하는 Exception 처리)
    CustomExceptionHandler.kt
    - RestControllerAdvice, ResponseEntity에 담아서 사용

---
+++ src/main/kotlin/com/backend/handler/CustomExceptionHandler.kt
@@ -0,0 +1,22 @@
+package com.backend.handler
+
+import com.backend.handler.ex.CustomApiException
+import com.backend.handler.ex.CustomValidationException
+import com.backend.utill.ResponseDto
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class CustomExceptionHandler {
+    @ExceptionHandler(CustomValidationException::class)
+    fun validationException(e: CustomValidationException): ResponseEntity<*> {
+        return ResponseEntity(ResponseDto(-1, e.message, e.errorMap), HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler(CustomApiException::class)
+    fun apiException(e: CustomApiException): ResponseEntity<*> {
+        return ResponseEntity(ResponseDto(-1, e.message, null), HttpStatus.BAD_REQUEST)
+    }
+}
