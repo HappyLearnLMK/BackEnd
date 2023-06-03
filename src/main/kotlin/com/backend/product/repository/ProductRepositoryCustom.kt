8a0f2280e493c991e0f5d0e67f7fd5620c785b0a
eco_min <guldor10.b@gmail.com>
2023-06-03 17:39:19 +0900

    feat(repository): product Paging 쿼리 및 service 추가
    - ProductRepositoryCustomImpl.kt
    product code, productName, originFilename, saveFilename
    을 가져오는 페이징 쿼리 작성

--- src/main/kotlin/com/backend/product/repository/ProductRepositoryCustom.kt
+++ src/main/kotlin/com/backend/product/repository/ProductRepositoryCustom.kt
@@ -1,5 +1,11 @@
 package com.backend.product.repository
 
+import com.backend.product.dto.req.CategoryReqDto
+import com.backend.product.dto.res.ProductPageResDto
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+
 interface ProductRepositoryCustom {
+    fun productPage(pageable: Pageable, categoryReqDto: CategoryReqDto): Page<ProductPageResDto>?
 
 }
