2e4eae9dde33c0bf9ad5e7a4c62d1ae7c1f6882b
eco_min <guldor10.b@gmail.com>
2023-06-03 17:39:58 +0900

    feat(controller): product Paging 쿼리 추가로 인한 controller 개발

--- src/main/kotlin/com/backend/product/service/ProductService.kt
+++ src/main/kotlin/com/backend/product/service/ProductService.kt
@@ -1,29 +1,39 @@
 package com.backend.product.service
 
+import com.backend.handler.ex.CustomApiException
 import com.backend.product.domain.Product
 import com.backend.product.domain.ProductOption
-import com.backend.product.dto.req.ProductReqDto
+import com.backend.product.dto.req.CategoryReqDto
+import com.backend.product.dto.req.ProductPageReqDto
+import com.backend.product.dto.req.ProductSaveReqDto
+import com.backend.product.dto.res.ProductPageResDto
+import com.backend.product.repository.CategoryRepository
 import com.backend.product.repository.ProductOptionRepository
 import com.backend.product.repository.ProductRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.multipart.MultipartFile
-import java.util.*
 
 @Service
 class ProductService(
     private val productRepository: ProductRepository,
     private val productOptionRepository: ProductOptionRepository,
+    private val categoryRepository: CategoryRepository,
 ) {
 
     @Transactional
-    fun saveProduct(productDto: ProductReqDto, file: MultipartFile?): Product {
-        val product = Product(UUID.randomUUID().toString(), productDto)
+    fun saveProduct(productDto: ProductSaveReqDto, file: MultipartFile?): Product {
+        val findCategory =
+            categoryRepository.findByMainCategoryAndMiddleCategory(productDto.mainCategory, productDto.middleCategory)
+                ?: throw CustomApiException("Category 가 존재하지 않습니다.")
+        val product = productDto.saveProduct(findCategory)
 
-        if (productDto.productOptionsReqDtos.isEmpty()) {
+        if (productDto.productOptionsReqDtoList.isEmpty()) {
             productRepository.save(product)
         } else {
-            val productOptions = productDto.productOptionsReqDtos.map { optionsDto ->
+            val productOptions = productDto.productOptionsReqDtoList.map { optionsDto ->
                 val productOption = ProductOption(optionsDto)
                 productOption.addProduct(product)
                 productOption
@@ -33,5 +43,11 @@ class ProductService(
             productOptionRepository.saveAll(productOptions)
         }
         return product
+    }
+
+    @Transactional
+    fun productPage(productPageReqDto: ProductPageReqDto, categoryReqDto: CategoryReqDto): Page<ProductPageResDto>? {
+        val pageRequest = PageRequest.of(productPageReqDto.page, productPageReqDto.size)
+        return productRepository.productPage(pageRequest, categoryReqDto)
     }
 }
